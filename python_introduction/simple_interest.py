dd5cd995a5aab1b843c6cd4ad87534a5e5191636
blaizre <jlego104@gmail.com>
2025-07-17 10:31:09 -0400

    updated the variable names.

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -4,6 +4,6 @@ principal = 1000 #in usd
 rate = 0.05 #annual interest rate
 time = 3 #in years
 
-I = principal*rate*time
+interest = principal*rate*time
 
-print(f"The simple interest is: {I}")
+print(f"The simple interest is: {interest}")
