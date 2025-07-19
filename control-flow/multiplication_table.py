996a8e565748b0aa544d9fcca36291d988206d95
blaizre <jlego104@gmail.com>
2025-07-19 05:24:04 -0400

    Added multiplication table.

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,9 @@
+#!/bin/bash/python3
+
+number = int(input("Enter a number to see its multiplication table: "))
+
+i = [1,2,3,4,5,6,7,8,9,10] 
+
+for a in i:
+    print(f"{number} * {a} = ", number * a)
+
