8db04256d2fdb0d3faaada3204587a57f22202c5
blaizre <jlego104@gmail.com>
2025-08-03 08:59:45 -0400

    shopping_list.

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -15,18 +15,35 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            pass
+            add_item = input("Enter item: ")
+            shopping_list.append(add_item)
+            print(f"'{add_item}' added to the list.")
+
         elif choice == '2':
             # Prompt for and remove an item
-            pass
+            remove_item = input("Enter an item to remove: ")
+            if remove_item in shopping_list:
+                shopping_list.remove(remove_item)
+                print(f"'{remove_item}' removed from the list.")
+            else:
+                print(f"'{remove_item}' not found in the shopping list.")
+
         elif choice == '3':
             # Display the shopping list
-            pass
+            if shopping_list:
+                print("Current Shopping List:")
+                for index, item in enumerate(shopping_list, 1):
+                    print(f"{index}. {item}")
+            else:
+                print("Your shopping list is empty.")
+
         elif choice == '4':
             print("Goodbye!")
             break
+
         else:
             print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
+
